d75b1dda42348c45a25cc6a4f5c830445a22035b
GAYATHRI20 <gayathrimuniraj27@gmail.com>
2017-06-07 11:20:12 +0530

    Add files via upload

---
+++ gg3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+ 
+
+int main() 
+{
+    
+int a,b,c;
+
+    printf("enter the values a b and c");
+
+    scanf("%d" "%d" "%d",&a,&b,&c);
+
+    if(a>b && a>c)
+    
+    {  
+ printf("a is grater");
+ 
+   }
+   
+ else if(a<b && b>c)
+  
+  {
+      
+  printf("b is grater");
+  
+  }
+   
+ else 
+   
+ {
+     
+   printf("c is grater");
+
+    }
+    
+
+    return 0;
+
+}
+ 
