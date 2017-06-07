139a421dddc93ddbf5190778863ce2b61eb7acd7
GAYATHRI20 <gayathrimuniraj27@gmail.com>
2017-06-07 10:21:14 +0530

    Add files via upload

---
+++ gg2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    char c;
+    printf("enter the alphapet");
+    scanf("%c",&c);
+    if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+    {
+        printf(" it is vowel");
+    }
+    else
+    {
+        printf("it is consonant");
+    }
+    return 0;
+}
+ 
