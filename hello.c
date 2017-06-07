1810c2c9dc23618a6adc15ccc0d52be28cd44032
GAYATHRI20 <gayathrimuniraj27@gmail.com>
2017-06-07 14:57:09 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+int main()
+ {
+	
+   int i,n;
+
+   printf("enter the number n");
+
+   scanf("%d",&n);
+
+	for(i=1;i<=n;i++)
+
+	{
+
+	    printf("\n HELLO");
+
+	}
+
+
+ }
