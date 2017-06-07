ca3d27d2118ea226fc42c2c9e1f7b322bf47515b
GAYATHRI20 <gayathrimuniraj27@gmail.com>
2017-06-07 14:40:15 +0530

    Add files via upload

---
+++ leap.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+int main()
+ {
+
+	int y;
+
+	printf("enter the year");
+
+	scanf("%d",&y);
+
+	if(y%4==0)
+
+	{
+	 
+          printf("this is leap year");
+
+	}
+	
+        else
+
+	{
+
+	    printf("this is not leap year");
+
+	}
+
+}
