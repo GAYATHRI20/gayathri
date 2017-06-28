fbe42a583416db9ab4ce5667d9d00033b8323955
GAYATHRI20 <gayathrimuniraj27@gmail.com>
2017-06-28 11:04:54 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,16 @@
+
+public class Odd 
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=6;i<=30;i++)
+		{
+		if(i%2!=0)
+		{
+		System.out.println(i);
+	}
+
+		}
+}
+}
