f27d868102b9f4be508db8613e58e29b1e67135e
GAYATHRI20 <gayathrimuniraj27@gmail.com>
2017-06-29 14:32:01 +0530

    Add files via upload

---
+++ StringConvert.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StringConvert {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str;
+System.out.println("enter the string");
+Scanner s=new  Scanner(System.in);
+str=s.nextLine();
+int i=Integer.parseInt(str);
+System.out.println(str+200);
+System.out.println(i+200);
+	}
+
+}
+
+
