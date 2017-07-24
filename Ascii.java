37365220671a97f811fc8b765c9a417576540be9
GAYATHRI20 <gayathrimuniraj27@gmail.com>
2017-07-24 10:08:44 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,12 @@
+package guvi;
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i;
+for(i=0;i<26;i++) {
+	System.out.println((char)('A'+i)+"--->"+('A'+i));
+}
+	}
+}
